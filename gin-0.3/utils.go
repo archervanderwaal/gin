29665dbe6f07897deb7b3b5279223b6534c146a2
archer <archer.vanderwaal@gmail.com>
2018-11-11 12:21:24 +0800

    gin-0.3阅读完成(重构自0.2b, 仅修改文件组织)

---
+++ gin-0.3/utils.go
@@ -0,0 +1,44 @@
+package gin
+
+// gin 框架源码阅读笔记
+// date: 2018/11/11
+// author: archer vanderwaal 一北@archer.vanderwaal@gmail.com
+import (
+	"encoding/xml"
+)
+
+type H map[string]interface{}
+
+// 抽取自gin.go-0.2b
+// Allows type H to be used with xml.Marshal
+func (h H) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	start.Name = xml.Name{
+		Space: "",
+		Local: "map",
+	}
+	if err := e.EncodeToken(start); err != nil {
+		return err
+	}
+	for key, value := range h {
+		elem := xml.StartElement{
+			Name: xml.Name{Space: "", Local: key},
+			Attr: []xml.Attr{},
+		}
+		if err := e.EncodeElement(value, elem); err != nil {
+			return err
+		}
+	}
+	if err := e.EncodeToken(xml.EndElement{Name: start.Name}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func filterFlags(content string) string {
+	for i, a := range content {
+		if a == ' ' || a == ';' {
+			return content[:i]
+		}
+	}
+	return content
+}
